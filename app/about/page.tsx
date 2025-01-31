fef38fd5dd34e94f2853f8c48f5b554fc382a5e4
Anup kumar <anoopkumar0145@gamil.com>
2025-01-31 15:37:42 +0530

    debug 3.1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,7 +17,7 @@ export default function about() {
     <div className="  p-2 flex items-center justify-center">
       <div className="w-[90%] xl:w-4/6 h-auto flex flex-col mb-10 pb-10">
 
-        <div className="border-1 w-full h-auto lg:h-[23rem] flex justify-between flex-col lg:flex-row   items-center">
+        <div className=" w-full h-auto lg:h-[23rem] flex justify-between flex-col lg:flex-row   items-center">
           <ContainerAbout images={myimage} />
           <div className="w-full lg:w-[65%] flex flex-col justify-between py-4">
             <h3 className="text-white text-4  xl xl:text-6xl flex justify-center items-center ">
